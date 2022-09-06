82ca9a2bfde4a08c42f28cf7433fe656eca0229e
Muhammad Waqar <muhammad.waqar@kryptomind.com>
2022-09-06 15:47:32 +0500

    changes

--- src/services/contract.service.ts
+++ src/services/contract.service.ts
@@ -5,11 +5,7 @@ const getNewWeb3 = (isMainnnet = false) => {
   return new Web3(new Web3.providers.HttpProvider(config.RPC_URL));
 };
 
-const initalizeContract = async (
-  abi: any[],
-  address: string | any,
-  isMainnnet = true
-) => {
+const initalizeContract = async (abi: any[], address: string | any) => {
   let web3 = new Web3(new Web3.providers.HttpProvider(config.RPC_URL));
   let res = new web3.eth.Contract(abi, address);
   return res;
