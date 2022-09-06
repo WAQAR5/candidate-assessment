12019dcf45defeefef44d983479919b1c5719410
Muhammad Waqar <muhammad.waqar@kryptomind.com>
2022-09-06 15:41:45 +0500

    structure changes

---
+++ src/services/contract.service.ts
@@ -0,0 +1,18 @@
+import Web3 from "web3";
+import config from "../utils/config";
+
+const getNewWeb3 = (isMainnnet = false) => {
+  return new Web3(new Web3.providers.HttpProvider(config.RPC_URL));
+};
+
+const initalizeContract = async (
+  abi: any[],
+  address: string | any,
+  isMainnnet = true
+) => {
+  let web3 = new Web3(new Web3.providers.HttpProvider(config.RPC_URL));
+  let res = new web3.eth.Contract(abi, address);
+  return res;
+};
+
+export { getNewWeb3, initalizeContract };
---
+++ src/utils/config.ts
@@ -0,0 +1,9 @@
+const mainnetConfig = {
+  RPC_URL: "https://polygon-rpc.com/ ",
+};
+
+const testnetConfig = {
+  RPC_URL: "https://rpc-mumbai.maticvigil.com/",
+};
+
+export default testnetConfig;
